347d7aebe474cfea5283ae7dec77af6037ce4e3e
human-one1 <s1101756@gm.pu.edu.tw>
2022-10-27 20:44:20 +0800

    make it better

---
+++ nwe.py
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="zh-TW">
+<head>
+	<meta charset="UTF-8">
+	<menta name = "viewport" concent = "width = device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	
+</body>
+</html>
